9fd3bf827edd76523e7f3856d0cba06203690a8e
Lars Jönsson <crymeat@gmail.com>
2021-03-22 12:07:11 +0100

    thrid

--- index.js
+++ index.js
@@ -7,35 +7,45 @@ function modelLoaded() {
 	getImage();
 }
 
-async function getImage() {
+async function getImage(tag) {
+	console.log(tag);
 	try {
 		const data = await fetch(
-			'https://api.giphy.com/v1/gifs/random?api_key=9KxUGtIb2nKpRE9G6h98Py2nqSi96Zls'
+			`https://api.giphy.com/v1/gifs/random?api_key=9KxUGtIb2nKpRE9G6h98Py2nqSi96Zls&tag=${tag}`
 		);
 		const res = await data.json();
 		const gif = res.data.image_original_url;
 		const app = document.getElementById('app');
 		const div = document.createElement('div');
 		const pictureElement = document.createElement('img');
+		const loading = document.getElementsByClassName('loading');
 		pictureElement.src = gif;
 		pictureElement.setAttribute('crossOrigin', 'anonymous');
 		pictureElement.id = 'gif';
 		app.appendChild(div);
 		div.appendChild(pictureElement);
+		if (pictureElement && loading[0]) {
+			console.log(loading[0]);
+			loading[0].classList.remove('loading');
+		}
 
 		pictureElement.addEventListener('load', () => {
 			// Make a prediction with a selected image
 			classifier.classify(pictureElement, (err, results) => {
 				const label = getLabels(results);
+
 				console.log(label);
-				//<img src="${pictureElement}" crossorigin="anonymous" />
 				const text = `
-		 <div>
-			${label.map((item) => `<button onClick={test()}>${item}</button>`).join(' ')}
-		</div>
-  `;
+				<div>
+				${label
+					.map(
+						(item) =>
+							`<button id="button" onClick="getImage('${item}')">${item}</button>`
+					)
+					.join(' ')}
+						</div>`;
 				const app = document.getElementById('app');
-				app.insertAdjacentHTML('afterend', text);
+				app.insertAdjacentHTML('beforeend', text);
 			});
 		});
 	} catch (error) {
@@ -43,12 +53,6 @@ async function getImage() {
 			`Something went wrong with the request to the giphy api, ${error}`
 		);
 	}
-}
-
-function classification(pictureElement) {}
-
-function test() {
-	console.log('he');
 }
 
 function getLabels(data) {
